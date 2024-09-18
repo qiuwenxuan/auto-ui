72327360df40fa678821e3d2296159c2130ba2e1
William Qiu <v-williamqiu@microsoft.com>
2024-09-18 15:21:59 +0800

    添加sqlite manager

--- config/conf.py
+++ config/conf.py
@@ -22,7 +22,8 @@ CURRENT_PATH = os.path.abspath(__file__)
 CONF_PATH = os.path.join(ROOT_DIR, 'config', 'config.ini')
 # 日志文件输出路径
 LOG_PATH = os.path.join(ROOT_DIR, 'report', 'log', 'app.log')
-
+# 数据库文件路径
+SQL_FILE = os.path.join(ROOT_DIR, 'db.sqlite3')
 # chrome driver路径
 CHROME_PATH = os.path.join(ROOT_DIR, 'driver', 'chromedriver.exe')
 # edge driver路径
@@ -32,11 +33,12 @@ if __name__ == '__main__':
     print('CURRENT_TIME:{}'.format(CURRENT_TIME))
     print('DATE_TIME:{}'.format(DATE_TIME))
     print('ROOT_DIR:{}'.format(ROOT_DIR))
-    print('LOG_DIR:{}'.format(LOG_PATH))
     print('SCREENSHOT_DIR:{}'.format(SCREENSHOT_DIR))
     print('REPORT_DIR:{}'.format(REPORT_DIR))
     print('DATA_DIR:{}'.format(DATA_DIR))
     print('CURRENT_PATH:{}'.format(CURRENT_PATH))
     print('CONF_PATH:{}'.format(CONF_PATH))
+    print('LOG_DIR:{}'.format(LOG_PATH))
+    print('SQL_FILE:{}'.format(SQL_FILE))
     print('CHROME_PATH:{}'.format(CHROME_PATH))
     print('EDGE_PATH:{}'.format(EDGE_PATH))
